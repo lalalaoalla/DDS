7f9bc0eac2529310338704a65c1439908222219b
lalalaoalla <avologina93@mail.ru>
2025-03-08 00:18:04 +0700

    Наладила ссылки

--- dds_project/dds_project/urls.py
+++ dds_project/dds_project/urls.py
@@ -23,6 +23,11 @@ from ddsBD.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('',index, name='index'),
+    #создание
     path('create/',create, name='create'),
+    path('success/', success_view, name='success_url'),
+    #редактирование
+    path('edit_dds/<int:id>/', edit_dds, name='edit_dds'),
+    #словарь
     path('guide/',guide,name='guide'),
 ]
