f958ec0222070abdafe33a91f675b80f6b5eae97
lalalaoalla <avologina93@mail.ru>
2025-03-08 00:19:34 +0700

    Создала и зарегистрировала сводную таблицу

--- dds_project/dds/admin.py
+++ dds_project/dds/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import DDS
 # Register your models here.
+
+#регистрация таблицы с информацией
+class DDSAdmin(admin.ModelAdmin):
+    list_display = ['created_at', 'status', 'typedds', 'category', 'subcategory', 'summa', 'comment']
+
+    # Поля, по которым можно фильтровать записи
+    list_filter = ['created_at', 'status', 'typedds', 'category', 'subcategory']
+
+    class Meta:
+        model= DDS
+
+admin.site.register(DDS,DDSAdmin)
--- dds_project/dds/models.py
+++ dds_project/dds/models.py
@@ -1,5 +1,25 @@
 from django.db import models
+from ddsBD.models import *
+from django.core.validators import MinValueValidator
 
-# Create your models here.
 
-    
+# класс выводимой информации  
+class DDS(models.Model):
+    created_at = models.DateField(verbose_name="Дата создания записи")
+    #так как в тз не сказано, что поле статус необязательно к заполнению, тогда пускай будет обязательно
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, verbose_name="Статус")
+    typedds = models.ForeignKey(Type,on_delete=models.CASCADE, blank=False, null=False, verbose_name="Тип")
+    category=models.ForeignKey(Category, on_delete=models.CASCADE, blank=False, null=False, verbose_name="Категория")
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, blank=False, null=False, verbose_name="Подкатегория")
+    summa=models.DecimalField(
+        max_digits=10, 
+        decimal_places=2, 
+        blank=False, 
+        null=False,
+        validators=[MinValueValidator(0.01)],
+        verbose_name="Сумма")
+    comment = models.TextField(blank=True, null=True, verbose_name="Комментарий")
+
+    class Meta:
+        verbose_name = "Данные о движении денежных средств"
+        verbose_name_plural = "Даннные о движениях денежных средств"
